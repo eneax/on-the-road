4bd53471f322fd962d7c43683b9bcea6e31f9f3c
eneax <19818937+eneax@users.noreply.github.com>
2019-07-12 10:02:02 +0200

    Intro to Gatsby Link

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -7,7 +8,8 @@ const Blog = () => (
   <Layout>
     <SEO title="Blog" />
     <h1>Blog</h1>
-    
+
+    <Link to="/">Back home</Link>
   </Layout>
 )
 
