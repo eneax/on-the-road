e6706724023d97640c302d0ba0bf471c65468cda
eneax <19818937+eneax@users.noreply.github.com>
2019-07-12 09:49:25 +0200

    Add tours page

---
+++ src/pages/tours.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const Tours = () => (
+  <Layout>
+    <SEO title="Tours" />
+    <h1>Tours</h1>
+    
+  </Layout>
+)
+
+export default Tours
