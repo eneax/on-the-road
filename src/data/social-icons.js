b60d803d84177f113387e9d11148a4746692bf8c
eneax <19818937+eneax@users.noreply.github.com>
2019-07-14 09:51:07 +0200

    Create data folder for constants

---
+++ src/data/social-icons.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { FaFacebook, FaInstagram, FaTwitter } from 'react-icons/fa'
+
+
+export default [
+  {
+    icon: <FaFacebook />,
+    url: 'https://facebook.com/'
+  },
+  {
+    icon: <FaInstagram />,
+    url: 'https://instagram.com/'
+  },
+  {
+    icon: <FaTwitter />,
+    url: 'https://twitter.com/'
+  },
+]
