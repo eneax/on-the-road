4f80856690f5321e206d587909ace7bc0f7c9fef
eneax <19818937+eneax@users.noreply.github.com>
2019-07-15 22:09:10 +0200

    Add hooks to navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,15 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'gatsby'
+import { FaAlignRight } from 'react-icons/fa'
 import styles from './navbar.module.css'
+import links from '../data/links'
+import socialIcons from '../data/socialIcons'
+import logo from '../images/logo.svg'
 
 const Navbar = () => {
+  const [isOpen, setNav] = (useState(false))
+
+  const toggleNav = () => {
+    setNav(isOpen => !isOpen)
+  }
+
+  console.log(isOpen);
+
   return (
     <div className={styles.navbar}>
-      <h1>navbar</h1>
+      <div className={styles.navCenter}>
+        <div className={styles.navHeader}>
+          <img src={logo} alt={`${logo} logo`} />
+          <button 
+            type="button" 
+            className={styles.logoBtn}
+            onClick={toggleNav}
+          >
+            <FaAlignRight className={styles.logoIcon}/>
+          </button>
+        </div>
 
-      <ul className={styles.links}>
-        <li>link one</li>
-        <li>link two</li>
-      </ul>
+        <ul className={isOpen ? `${styles.navLinks} ${styles.showNav}` : `${styles.navLinks}`}>
+          {links.map((item, i) => (
+            <li key={i}>
+              <Link to={item.path}>
+                {item.text}
+              </Link>
+            </li>
+          ))}
+        </ul>
+
+        <div className={styles.navSocialLinks}>
+          {socialIcons.map((item, i) => (
+            <a key={i} href={item.url} target="_blank" rel="noopener noreferrer">
+              {item.icon}
+            </a>
+          ))}
+        </div>
+      </div>
     </div>
   )
 }
