9cc6f43f541977ac4425823e969cd966fc331506
eneax <19818937+eneax@users.noreply.github.com>
2019-07-16 20:44:45 +0200

    Refactor footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,9 +1,30 @@
 import React from 'react'
+import { Link } from 'gatsby'
+
+import styles from './footer.module.css'
+import links from '../data/links'
+import socialIcons from '../data/socialIcons'
 
 const Footer = () => {
   return (
-    <footer>
-      © {new Date().getFullYear()}
+    <footer className={styles.footer}>
+      <div className={styles.links}>
+        {links.map((link, i) => (
+          <Link key={i} to={link.path}>
+            {link.text}
+          </Link>
+        ))}
+      </div>
+      <div className={styles.icons}>
+        {socialIcons.map((item, i) => (
+          <a href={item.url} key={i} target='_blank' rel="noopener noreferrer">
+            {item.icon}
+          </a>
+        ))}
+      </div>
+      <div className={styles.copyright}>
+        On the Road Inc. © {new Date().getFullYear()}
+      </div>
     </footer>
   )
 }
