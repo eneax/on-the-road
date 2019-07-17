941b9de75149df3fae40418a05036e3cb1f9260c
eneax <19818937+eneax@users.noreply.github.com>
2019-07-17 22:31:40 +0200

    Add banner to homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,13 +4,26 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Hero from "../components/hero"
+import Banner from "../components/banner"
 
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     
-    <Hero />
+    <Hero>
+      <Banner 
+        title="Keep Exploring" 
+        info='Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, obcaecati numquam! Repellendus sit rem.'
+      >
+        <Link 
+          to='/tours'
+          className='btn-white'
+        >
+          explore tours
+        </Link>
+      </Banner>
+    </Hero>
   </Layout>
 )
 
