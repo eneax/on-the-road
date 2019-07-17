4d5587613298f55d4f80ff51c7039ecd86f9c879
eneax <19818937+eneax@users.noreply.github.com>
2019-07-17 22:40:05 +0200

    Refactor 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,27 @@
 import React from "react"
+import { Link } from 'gatsby'
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Banner from "../components/banner"
+import styles from '../components/error.module.css'
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    
+    <header className={styles.error}>
+      <Banner
+        title="ooops...it's a dead end"
+      >
+        <Link 
+          to='/'
+          className='btn-white'
+        >
+          Back to home page
+        </Link>
+      </Banner>
+    </header>
   </Layout>
 )
 
