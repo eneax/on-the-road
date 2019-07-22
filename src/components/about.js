894a1d35c20d06730beb94cc6d3a991e1f93b6d6
eneax <19818937+eneax@users.noreply.github.com>
2019-07-22 20:44:27 +0200

    Refactor about section

--- src/components/about.js
+++ src/components/about.js
@@ -1,13 +1,29 @@
 import React from 'react'
 import Title from '../components/title'
+import styles from '../components/about.module.css'
+import img from '../images/defaultBcg.jpeg'
 
 const About = () => {
   return (
-    <div>
-      <p>hello</p>
-
+    <section className={styles.about}>
       <Title title='about' subtitle='us' />
-    </div>
+
+      <div className={styles.aboutCenter}>
+        <article className={styles.aboutImg}>
+          <div className={styles.imgContainer}>
+            <img src={img} alt="On the road background"/>
+          </div>
+        </article>
+        <article className={styles.aboutInfo}>
+          <h4>Explore the difference</h4>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint ex nobis, quam libero, adipisci ratione, aliquid numquam provident officia officiis maxime accusantium. Dolorem est sed culpa veritatis ducimus iste porro.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint ex nobis, quam libero, adipisci ratione, aliquid numquam provident officia officiis maxime accusantium. Dolorem est sed culpa veritatis ducimus iste porro.</p>
+          <button type='button' className='btn-primary'>
+            Read More
+          </button>
+        </article>
+      </div>
+    </section>
   )
 }
 
