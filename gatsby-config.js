75c4376fbd687f4f906e65d1fd4e6fdef30d0ed5
eneax <19818937+eneax@users.noreply.github.com>
2019-07-28 21:18:00 +0200

    Update siteMetadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `On the Road`,
+    description: `It's time to hit the road for some adventures!`,
+    author: `@eneax`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
