873d3d4392f9d032ca47852d2fe3207ba8c12422
eneax <19818937+eneax@users.noreply.github.com>
2019-08-07 21:09:04 +0200

    Add bcg image to contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,14 +1,33 @@
 import React from "react"
+import { graphql } from "gatsby";
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import HeroStyled from "../components/heroStyled"
 
-const Contact = () => (
+const Contact = ({ data }) => (
   <Layout>
     <SEO title="Contact" />
-    <h1>Contact</h1>
+
+    <HeroStyled
+      img={data.contactBcg.childImageSharp.fluid}
+    >
+      <h1>Contact</h1>
+    </HeroStyled>
     
   </Layout>
 )
+
+export const query = graphql`
+  {
+    contactBcg: file(relativePath: {eq: "connectBcg.jpeg"}) {
+      childImageSharp {
+        fluid(quality: 90, maxWidth: 4160) {
+          ...GatsbyImageSharpFluid_withWebp
+        }
+      }
+    }
+  }
+`
 
 export default Contact
