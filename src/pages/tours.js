dee25402f6739c1ebc94e6d3a6649e4ec4379c95
eneax <19818937+eneax@users.noreply.github.com>
2019-08-07 21:07:59 +0200

    Refactor tours and add bcg image

--- src/pages/tours.js
+++ src/pages/tours.js
@@ -1,14 +1,37 @@
-import React from "react"
+import React, { Component } from "react"
+import { graphql } from "gatsby";
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import HeroStyled from "../components/heroStyled"
 
-const Tours = () => (
-  <Layout>
-    <SEO title="Tours" />
-    <h1>Tours</h1>
-    
-  </Layout>
-)
+class Tours extends Component {
+  render() {
+    return (
+      <Layout>
+        <SEO title="Tours" />
+
+        <HeroStyled
+          img={this.props.data.toursBcg.childImageSharp.fluid}
+        >
+          <h1>Tours</h1>
+        </HeroStyled>
+      </Layout>
+    )
+  }
+}
+
+export const query = graphql`
+  {
+    toursBcg: file(relativePath: {eq: "defaultBcg.jpeg"}) {
+      childImageSharp {
+        fluid(quality: 90, maxWidth: 4160) {
+          ...GatsbyImageSharpFluid_withWebp
+        }
+      }
+    }
+  }
+`
+
 
 export default Tours
