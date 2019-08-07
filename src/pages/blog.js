e668d64a89963f0026c0265a3d20a688d1312ea8
eneax <19818937+eneax@users.noreply.github.com>
2019-08-07 21:08:38 +0200

    Add bcg image to blog page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,16 +1,34 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import HeroStyled from "../components/heroStyled"
 
-const Blog = () => (
+const Blog = ({ data }) => (
   <Layout>
     <SEO title="Blog" />
-    <h1>Blog</h1>
+
+    <HeroStyled
+      img={data.blogBcg.childImageSharp.fluid}
+    >
+      <h1>Blog</h1>
+    </HeroStyled>
 
     <Link to="/">Back home</Link>
   </Layout>
 )
+
+export const query = graphql`
+  {
+    blogBcg: file(relativePath: {eq: "blogBcg.jpeg"}) {
+      childImageSharp {
+        fluid(quality: 90, maxWidth: 4160) {
+          ...GatsbyImageSharpFluid_withWebp
+        }
+      }
+    }
+  }
+`
 
 export default Blog
