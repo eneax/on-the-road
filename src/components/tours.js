e69653001995552758f19898d9a5c6e040ace796
eneax <19818937+eneax@users.noreply.github.com>
2019-08-13 23:17:57 +0600

    Add graphql query to tours component

--- src/components/tours.js
+++ src/components/tours.js
@@ -1,14 +1,37 @@
 // Tours components handles data with graphql, while TourList deals with the view
 import React from 'react'
+import {useStaticQuery, graphql} from 'gatsby'
+
 import TourList from './tourList'
 
-const Tours = () => {
-  return (
-    <div>
-      this is Tours
+const getTours = graphql`
+  query {
+    tours: allContentfulTour {
+      edges {
+        node {
+          contentful_id
+          name
+          price
+          slug
+          country
+          days
+          images {
+            fluid {
+              ...GatsbyContentfulFluid_tracedSVG
+            }
+          }
+        }
+      }
+    }
+  }
+`
 
-      <TourList />
-    </div>
+const Tours = () => {
+  const tours = useStaticQuery(getTours)
+  console.log(tours)
+
+  return (
+    <TourList tours={tours} />
   )
 }
 
