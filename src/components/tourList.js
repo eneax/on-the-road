3619a9d8b818379e62132c055f86bc1d79e9822f
eneax <19818937+eneax@users.noreply.github.com>
2019-08-13 23:18:29 +0600

    Add state to tourList component

--- src/components/tourList.js
+++ src/components/tourList.js
@@ -1,11 +1,40 @@
-import React from 'react'
+// here we do a similar approach used for FeaturedTours
+import React, { Component } from 'react'
 
-const TourList = () => {
-  return (
-    <div>
-      this is TourList
-    </div>
-  )
+import styles from './items.module.css'
+import Tour from './tour'
+import Title from './title'
+import { node } from 'prop-types';
+
+export default class TourList extends Component {
+  state = {
+    tours: [],
+    sortedTours: [], // if I want to implement a filter in the future
+  }
+
+  componentDidMount() {
+    this.setState({
+      tours: this.props.tours.edges,
+      sortedTours: this.props.tours.edges,
+    })
+  }
+
+  render() {
+    return (
+      <section className={styles.tours}>
+        <Title title='our' subtitle='tours' />
+
+        <div className={styles.center}>
+          {this.state.tours.map(({node}) => {
+            return (
+              <Tour 
+                key={node.contentful_id}
+                tour={node}
+              />
+            )
+          })}
+        </div>
+      </section>
+    )
+  }
 }
-
-export default TourList
