93aa26b38fe830e81b80bce10337b17d2086d91e
eneax <19818937+eneax@users.noreply.github.com>
2019-08-18 22:29:55 +0200

    Create pages programmatically

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,27 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require('path')
 
-// You can delete this file if you're not using it
+exports.createPages = async({ graphql, actions }) => {
+  const { createPage } = actions
+
+  const { data } = await graphql(`
+    query {
+      tours: allContentfulTour {
+        edges {
+          node {
+            slug
+          }
+        }
+      }
+    }  
+  `)
+
+  data.tours.edges.forEach(({node}) => {
+    createPage({
+      path: `tours/${node.slug}`,
+      component: path.resolve('./src/templates/tourTemplate.js'),
+      context: {
+        slug: node.slug,
+      }
+    })
+  })
+}
