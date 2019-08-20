9d16e82237b1b00e5fba77e6fed085e43ba06d71
eneax <19818937+eneax@users.noreply.github.com>
2019-08-20 21:26:01 +0200

    Create query in TourTemplate

--- src/templates/tourTemplate.js
+++ src/templates/tourTemplate.js
@@ -1,11 +1,40 @@
 import React from 'react'
+import { graphql } from 'gatsby'
 
-const TourTemplate = () => {
+const TourTemplate = ({ data }) => {
+  const {name, price, country, days, start, description: {description}, journey, images} = data.tour
+
+  const [mainImage, ...tourImages] = images
+
   return (
     <div>
-      Dummy data template
+      <h1>{name}</h1>
     </div>
   )
 }
+
+export const query = graphql`  
+  query($slug: String!) { # from gatsby-node --> context: { slug: node.slug } 
+    tour: contentfulTour(slug: {eq:$slug}) {
+      name
+      price
+      country
+      days
+      start(formatString: "dddd MMMM Do, YYYY")
+      description{
+        description
+      }
+      journey{
+        day
+        id
+      }
+      images{
+        fluid{
+          src
+        }
+      }
+    }
+  }
+`
 
 export default TourTemplate
