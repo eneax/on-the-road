630a574074d51aed3ab774c7bb5394f674e3f70b
eneax <19818937+eneax@users.noreply.github.com>
2019-08-22 21:54:19 +0200

    Config BlogCard

--- src/components/blogCard.js
+++ src/components/blogCard.js
@@ -1,10 +1,26 @@
-import React from 'react'
+import React from "react"
+import Image from "gatsby-image"
+import AniLink from "gatsby-plugin-transition-link/AniLink"
 
-const BlogCard = () => {
+import styles from "./blogCard.module.css"
+
+
+const BlogCard = ({ blog }) => {
+  const { slug, title, image, published } = blog
+  
   return (
-    <div>
-      BlogCard
-    </div>
+    <article className={styles.blog}>
+      <div className={styles.imgContainer}>
+        <Image fluid={image.fluid} className={styles.img} alt="single post" />
+        <AniLink fade className={styles.link} to={`/blog/${slug}`}>
+          read more
+        </AniLink>
+        <h6 className={styles.date}>{published}</h6>
+      </div>
+      <div className={styles.footer}>
+        <h4>{title}</h4>
+      </div>
+    </article>
   )
 }
 
