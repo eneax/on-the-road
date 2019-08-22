85b584990f555a63dd9c3775a2695fe3fb49952c
eneax <19818937+eneax@users.noreply.github.com>
2019-08-22 21:36:07 +0200

    Config BlogList query

--- src/components/blogList.js
+++ src/components/blogList.js
@@ -1,12 +1,51 @@
 import React from 'react'
+import {useStaticQuery, graphql} from 'gatsby'
+
+import Title from './title'
 import BlogCard from './blogCard'
 
+import styles from './blog.module.css'
+
+
+const getPosts = graphql`
+  {
+    posts: allContentfulPost(sort: {fields: published, order: DESC}) {
+      edges {
+        node {
+          published(formatString: "MMMM Do, YYYY")
+          createdAt(formatString: "LLLL")
+          title
+          slug
+          id: contentful_id
+          image {
+            fluid {
+              ...GatsbyContentfulFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
 const BlogList = () => {
+  const { posts } = useStaticQuery(getPosts)
+
   return (
-    <div>
-      BlogList
-      <BlogCard />
-    </div>
+    <section className={styles.blog}>
+      <Title title='our' subtitle='blog' />
+
+      <div className={styles.center}>
+        {
+          posts.edges.map(({node}) => (
+            <BlogCard 
+              key={node.id}
+              blog={node}
+            />
+          ))
+        }
+      </div>
+    </section>
   )
 }
 
