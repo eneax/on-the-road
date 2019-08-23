7897787c215b475a0b9ed3acfe963ba3a5f92beb
eneax <19818937+eneax@users.noreply.github.com>
2019-08-23 21:17:28 +0200

    Generate blog pages dynamically

--- gatsby-node.js
+++ gatsby-node.js
@@ -12,6 +12,13 @@ exports.createPages = async({ graphql, actions }) => {
           }
         }
       }
+      posts: allContentfulPost {
+        edges {
+          node {
+            slug
+          }
+        }
+      }
     }  
   `)
 
@@ -19,6 +26,16 @@ exports.createPages = async({ graphql, actions }) => {
     createPage({
       path: `tours/${node.slug}`,
       component: path.resolve('./src/templates/tourTemplate.js'),
+      context: {
+        slug: node.slug,
+      }
+    })
+  })
+
+  data.posts.edges.forEach(({ node }) => {
+    createPage({
+      path: `blog/${node.slug}`,
+      component: path.resolve('./src/templates/blogTemplate.js'),
       context: {
         slug: node.slug,
       }
