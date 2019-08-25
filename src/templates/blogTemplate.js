e2f78ba043d92c2583fb103aed08fd084fb9acc9
eneax <19818937+eneax@users.noreply.github.com>
2019-08-25 12:35:15 +0200

    Render images in rich text field

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -9,6 +9,19 @@ import Layout from '../components/layout'
 
 const BlogTemplate = ({ data }) => {
   const { title, published, text: {json} } = data.post
+  const options  = {
+    renderNode: {
+      "embedded-asset-block": (node) => {
+        return (
+          <div className='rich'>
+            <h3>coooool image</h3>
+            <img width="400" src={node.data.target.fields.file['en-US'].url} alt=""/>
+            <p>image provided by john doe</p>
+          </div>
+        )
+      }
+    }
+  }
 
   return (
     <Layout>
@@ -17,7 +30,7 @@ const BlogTemplate = ({ data }) => {
           <h1>{title}</h1>
           <h4>published at: {published}</h4>
           <article className={styles.post}>
-            {documentToReactComponents(json)}
+            {documentToReactComponents(json, options)}
           </article>
 
           <AniLink fade to='/blog' className='btn-primary'>all posts</AniLink>
