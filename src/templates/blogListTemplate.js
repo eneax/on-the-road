87f12bcd163227837230d2bc5c4d17dd5e7eec90
eneax <19818937+eneax@users.noreply.github.com>
2019-08-26 21:53:25 +0200

    Create blogListTemplate

---
+++ src/templates/blogListTemplate.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+import AniLink from 'gatsby-plugin-transition-link/AniLink'
+
+import Layout from '../components/layout'
+import BlogCard from '../components/blogCard'
+import Title from '../components/title'
+import styles from './blog.module.css'
+
+
+const BlogListTemplate = () => {
+  return (
+    <div>
+      <p>BlogListTemplate</p>
+    </div>
+  )
+}
+
+export default BlogListTemplate
