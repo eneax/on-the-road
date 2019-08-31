34da240fd2163c2ce860e19b61771fa9b751b551
eneax <19818937+eneax@users.noreply.github.com>
2019-08-31 10:37:47 +0200

    Add data to blogListTemplate

--- src/templates/blogListTemplate.js
+++ src/templates/blogListTemplate.js
@@ -8,12 +8,47 @@ import Title from '../components/title'
 import styles from './blog.module.css'
 
 
-const BlogListTemplate = () => {
+const BlogListTemplate = (props) => {
+  const { data } = props
+
   return (
-    <div>
-      <p>BlogListTemplate</p>
-    </div>
+    <section className={styles.blog}>
+      <Title title='latest' subtitle='posts' />
+
+      <div className={styles.center}>
+        {data.posts.edges.map(({node}) => {
+          return (
+            <BlogCard 
+              key={node.id}
+              blog={node}
+            />
+          )
+        })}
+      </div>
+    </section>
   )
 }
 
 export default BlogListTemplate
+
+
+// query
+export const query = graphql`
+  query getPosts($skip: Int!, $limit: Int!) {
+    posts: allContentfulPost(skip: $skip, limit: $limit, sort: {fields: published, order: DESC}) {
+      edges {
+        node {
+          slug
+          title
+          id: contentful_id
+          published(formatString:"MMMM Do, YYYY")
+          image{
+            fluid{
+              ...GatsbyContentfulFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`
